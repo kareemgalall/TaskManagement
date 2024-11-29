e480634eb7e87ee2d290efc0ddd3ceccc7cbdcc4
kareemgalall <kareemgalal1890@gmail.com>
2024-11-29 14:04:15 +0200

    implemented input validation for task

--- src/main/java/banquemisr/challenge05/taskmanagement/domain/model/TaskEntity.java
+++ src/main/java/banquemisr/challenge05/taskmanagement/domain/model/TaskEntity.java
@@ -1,9 +1,14 @@
 package banquemisr.challenge05.taskmanagement.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -16,10 +21,16 @@ public class TaskEntity {
     @Id
     @GeneratedValue
     Long id;
+    @NotEmpty(message = "title cannot be empty")
     String title;
+    @NotEmpty(message="description cannot be empty")
     String description;
+    @Pattern(regexp = "LOW|MEDIUM|HIGH", message = "Status must be one of: LOW, MEDIUM, or HIGH")
     String priority;
+    @NotNull(message = "status cannot be null")
+    @Pattern(regexp = "NEW|IN_PROGRESS|COMPLETED", message = "Status must be one of: NEW, IN_PROGRESS, or COMPLETED")
     String status;
+    @Temporal(TemporalType.DATE)
     Date dueDate;
     @ManyToOne
     UserEntity user;
