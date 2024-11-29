2494db3d2d139effae576897a1620d186f4254ae
kareemgalall <kareemgalal1890@gmail.com>
2024-11-29 14:03:48 +0200

    added exception handler

---
+++ src/main/java/banquemisr/challenge05/taskmanagement/advice/ApplicationExceptionHandler.java
@@ -0,0 +1,27 @@
+package banquemisr.challenge05.taskmanagement.advice;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Map<String, String> handleInvalidArgument(ConstraintViolationException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            errorMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+        });
+        return errorMap;
+    }
+
+
+}
