51ac55c5bfd8990cf892c387f136fec2534af517
kareemgalall <kareemgalal1890@gmail.com>
2024-11-30 22:35:53 +0200

    renamed history class

--- src/main/java/banquemisr/challenge05/taskmanagement/domain/model/HistoryEntity.java
+++ src/main/java/banquemisr/challenge05/taskmanagement/domain/model/HistoryEntity.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class History {
+public class HistoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
