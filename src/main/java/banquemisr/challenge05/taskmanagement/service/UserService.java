e0bac4f2b27365aa9b19f0329a18f0e24ee47f74
kareemgalall <kareemgalal1890@gmail.com>
2024-11-30 17:17:00 +0200

    changed exception type in method signature

--- src/main/java/banquemisr/challenge05/taskmanagement/service/UserService.java
+++ src/main/java/banquemisr/challenge05/taskmanagement/service/UserService.java
@@ -1,12 +1,16 @@
 package banquemisr.challenge05.taskmanagement.service;
 
 import banquemisr.challenge05.taskmanagement.domain.model.UserEntity;
+import banquemisr.challenge05.taskmanagement.exception.AuthorizationException;
+import banquemisr.challenge05.taskmanagement.exception.PasswordInCorrectException;
 import banquemisr.challenge05.taskmanagement.exception.UserNotFoundException;
 
 public interface UserService {
-    String authenticate(UserEntity userDto);
+    String authenticate(UserEntity userDto) throws UserNotFoundException;
 
     UserEntity register(UserEntity userDto);
 
     UserEntity getProfile() throws UserNotFoundException;
+
+    UserEntity fullUpdate(Long id,UserEntity userEntity) throws UserNotFoundException, PasswordInCorrectException, AuthorizationException;
 }
